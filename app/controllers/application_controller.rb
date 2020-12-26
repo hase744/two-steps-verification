04a84add344a84976272e3885e4c668fef960320
hase744 <jugglego744@gmail.com>
2020-12-26 14:34:24 +0900

    ログアウト先追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def after_sign_out_path_for(resource)
+        #リダイレクト先のパス
+        #redirect_to 
+        "/pages/after_sign_out"
+        #new_accounts_session_path
+        #accounts_path
+      end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def after_sign_out
+  end
 end
